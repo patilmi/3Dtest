c2409075ac0ac7ba818fc319de047c3c74ee46ea
patilmi <97363699+patilmi@users.noreply.github.com>
2022-02-02 18:25:55 -0500

    Incremental rotation (#1)
    * Working 3d rotation through matrix transformation
    
    * Removed whitespace
    
    Co-authored-by: Mihir Patil <patilmi@umich.edu>

--- Assets/Scenes/GenerateBalls.cs
+++ Assets/Scenes/GenerateBalls.cs
@@ -7,29 +7,45 @@ public class GenerateBalls : MonoBehaviour
 {
     // Start is called before the first frame update
     
-    public int width = 9;
-    public int height = 9;
+    public int height = 6;
+    //public float speed;
+
+    public float alpha = 3;
+    public float beta = 2;
+    public float gamma = 7;
+
+
+   
 
     List<GameObject> ballList = new List<GameObject>();
 
     void Start()
     {
+        height = 3;
+        alpha = 3;
+        beta = 2;
+        gamma = 7;
         GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
         //sphere.AddComponent<Animation>();
         //sphere.AddComponent<MeshFilter>();
         //sphere.AddComponent<MeshCollider>();
         //sphere.AddComponent<MyCustomScript>();
         //sphere.AddComponent<MeshRenderer>();
+        //speed = 5f;
 
         var sphereRenderer = sphere.GetComponent<Renderer>();
 
         sphereRenderer.material.SetColor("_Color", Color.red);
         
-        for (int y=0; y<height; y+=2)
+        for (int y=-3; y <= height; y+=3)
         {
-           for (int x=0; x<width; x+=2)
+           for (int x=-3; x <= height; x+=3)
            {
-             ballList.Add(Instantiate(sphere, new Vector3(x,y,0), Quaternion.identity));
+                for (int z = -3; z <= height; z+=3)
+                {
+                    ballList.Add(Instantiate(sphere, new Vector3(x, y, z), Quaternion.identity));
+                }
+             
            }
         }    
 
@@ -37,9 +53,29 @@ public class GenerateBalls : MonoBehaviour
     }
 
     // Update is called once per frame
-    // void Update()
-    // {
+    void Update()  
+    {
+        float x, y, z;
+        float speed = 0.001f;
+        float factor = Time.deltaTime * speed;
+        for (int i = 0; i < ballList.Count; i++)
 
-        
-    // }
+        {
+            x = ballList[i].transform.position.x;
+            y = ballList[i].transform.position.y;
+            z = ballList[i].transform.position.z;
+            for (int j = 0; j < 100; j++)
+            {
+                x = x + factor * (alpha * y + beta * z);
+                y = y + factor * (gamma * z - alpha * x);
+                z = z - factor * (gamma * y + beta * x);
+
+            }
+            ballList[i].transform.position = new Vector3(x, y, z);
+
+            //ballList[i].transform.position = new Vector3(x + factor * (alpha * y + beta * z),
+            //    y + factor * (gamma * z - alpha * x), z - factor * (gamma * y + beta * x));
+        }
+
+    }
 }
